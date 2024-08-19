5402c38eec4bbb2e6b56ca7d56c182cfafe352ad
Erik Langendijk <erik.langendijk@gmail.com>
2024-08-19 15:35:58 +0200

    - Finale testen toegevoegd aan frontend

--- Erfpacht058-Web/src/app/app.component.spec.ts
+++ Erfpacht058-Web/src/app/app.component.spec.ts
@@ -1,10 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HttpHelperService } from './base/services/http-helper.service';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('AppComponent', () => {
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: []
     }).compileComponents();
   });
 
